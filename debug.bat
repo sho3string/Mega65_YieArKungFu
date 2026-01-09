967eee75e1dbe3632603d1384e198d413bfd9056
Samuel <36328867+sho3string@users.noreply.github.com>
2026-01-09 17:32:49 +1100

    Project files

---
+++ debug.bat
@@ -0,0 +1,4 @@
+cls
+pushd "G:\Mega65\YieArKungFu\Source\asm"
+"G:\Mega65\M65 Debugger\m65dbg.exe" -l tcp
+popd
---
+++ run.bat
@@ -0,0 +1,2 @@
+cmd /c "G:\Mega65\Xemu\xmega65.exe" -8 G:\Mega65\YieArKungFu\Yiearkf.d81 -uartmon :4510
+G:\Mega65\m65tools\etherload.exe -5 -m "YIEARKF.D81" -r "YIEARKUNGFU.PRG"
