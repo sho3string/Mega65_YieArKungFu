b265530df6676bdce961132b92c4ef4766861d05
Samuel <36328867+sho3string@users.noreply.github.com>
2026-03-19 18:21:46 +1100

    Update build.bat
    Build with latest tilesheet format.

--- build.bat
+++ build.bat
@@ -9,9 +9,13 @@ cmd /c copy "G:\Mega65\YiearKungFu\Source\asm\colorram.bin" "G:\Mega65\YiearKung
 
 rem cmd /c G:\Mega65\YiearKungFu\bin\exomizer.exe sfx 0x22b8 -x "LDA #$0B STA $D011 LDA $D020 EOR #$05 STA $D020 STA $D418" -t 65 -Di_ram_exit=0 -o Yiearkungfu.prg Yiearkf.prg 
 
-rem cmd /c copy "G:\Mega65\Exciting Hour\Source\asm\testp.prg" "G:\Mega65\Exciting Hour\testp.prg"
 cmd /c del "G:\Mega65\YiearKungFu\Yiearkf.d81"
-cmd /c G:\Mega65\Dev\bin\cc1541.exe -n "yie ar kung fu" -w "G:\Mega65\YiearKungFu\yiearkungfu.prg" -w "G:\Mega65\YiearKungFu\YiearKungFu\assets\tilesheet.chr" -w "G:\Mega65\YiearKungFu\colorram.bin" "G:\Mega65\YiearKungFu\Yiearkf.d81"
+
+echo Convert sprites.bin
+python G:\Mega65\YieArKungFu\YieArKungFu\Tools\patch_diagonals.py G:\Mega65\YieArKungFu\YieArKungFu\assets\TILESHEET.chr G:\Mega65\YieArKungFu\YieArKungFu\assets\TS.chr --tiles-per-row 16 --start-row 32 --sprite-cols 8 --sprite-rows 64
+
+
+cmd /c G:\Mega65\Dev\bin\cc1541.exe -n "yie ar kung fu" -w "G:\Mega65\YiearKungFu\yiearkungfu.prg" -w "G:\Mega65\YiearKungFu\YiearKungFu\assets\ts.chr" -w "G:\Mega65\YiearKungFu\colorram.bin" "G:\Mega65\YiearKungFu\Yiearkf.d81"
 cmd /c G:\Mega65\m65tools\mega65_ftp -e -c "put Yiearkf.D81"
 popd
 rem -x "LDA #$0B STA $D011 LDA $D020 EOR #$05 STA $D020 STA $D418"
