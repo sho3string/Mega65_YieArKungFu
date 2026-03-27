fe38e7323cc4f70ec3c61718a306ee91f1c1223b
Samuel <36328867+sho3string@users.noreply.github.com>
2026-03-27 23:54:40 +1100

    Update Constants.asm

--- Source/asm/Constants.asm
+++ Source/asm/Constants.asm
@@ -10,7 +10,7 @@
 .const SPRITE_MAX				= 23	 									// Maximum # of sprites to queue
 .const PIXIE_MAX				= 185
 .const SPRITE_Q_ENTRY_SIZE		= 4											// 4 bytes per sprite. ( Y, X, TILE, ATTR )
-.const SPRITE_Q_SIZE			= 1 + (SPRITE_MAX * SPRITE_Q_ENTRY_SIZE) // 23 x 4 = 92 bytes 
+.const SPRITE_Q_SIZE				= 1 + (SPRITE_MAX * SPRITE_Q_ENTRY_SIZE) // 23 x 4 = 92 bytes 
 /*
 On arcade.
 
@@ -23,36 +23,32 @@ byte 1 - character code LSB
 
 // - work ram 0x5000 - 0x57ff
 .const SPRITE_RAM1		= $8000
-.const WORK_RAM1		= $8030
-.const CMD_QUEUE		= $82C0
+.const WORK_RAM1			= $8030
+.const CMD_QUEUE			= $82C0
 .const SPRITE_RAM2		= $8400
-.const WORK_RAM2		= $8430
+.const WORK_RAM2			= $8430
 .const SCREEN_BASE		= $2400	 /* background 8x8 screen ram - physcially on screen top left at 5880*/
 .const ARCADE_VRAM_BASE	= $5800  /* Arcade character ram */
-.const SCREEN_WIDTH 	= 256	 /* arcade is 256 - 32 characters visible */
+.const SCREEN_WIDTH 		= 256	 /* arcade is 256 - 32 characters visible */
 .const SCREEN_HEIGHT 	= 256	 /* arcade is 224 - 28 characters visible, however visble portion starts at 0x5880, non visible at 0x5800 to 0x587f */
 .const CHARS_WIDE 		= (SCREEN_WIDTH / 8) 				// 32 characters.
 .const CHARS_HIGH 		= (SCREEN_HEIGHT / 8)				// 32 characters, 28 visible.
-.const TOTAL_CHARS  	= CHARS_WIDE + RRB_Tail_words   
+.const TOTAL_CHARS  		= CHARS_WIDE + RRB_Tail_words   
 .const LINESTEP_BYTES 	= TOTAL_CHARS * 2 
-.const LOGICAL_WIDTH	= (CHARS_WIDE << 1) + (RRB_Tail_words << 1 ) // 64 for characters + 96 for pixies. 2 bytes for each character and pixie.
-.const ROW_STRIDE		= $40 + (RRB_Tail_words * 2)
-.const COLOR_RAM		= $FF80000
-//.const LOADADDR		= $40000			// use spare ram to load stuff into.
-.const GRAPHMEM  		= $20000 			// this will be our character generator at bank 2
+.const LOGICAL_WIDTH		= (CHARS_WIDE << 1) + (RRB_Tail_words << 1 ) // 64 for characters + 96 for pixies. 2 bytes for each character and pixie.
+.const ROW_STRIDE			= $40 + (RRB_Tail_words * 2)
+.const COLOR_RAM			= $FF80000 
+//.const LOADADDR			= $40000			// use spare ram to load stuff into.
+.const GRAPHMEM  			= $20000 			// this will be our character generator at bank 2
 .const TILE_OFFSET		= GRAPHMEM/64
-//.const MEMBANK		= LOADADDR>>16		// 0x40000 >> 16 = 4
+//.const MEMBANK			= LOADADDR>>16		// 0x40000 >> 16 = 4
 .const TAIL_OFF			= CHARS_WIDE*2
-.const arcadeRowSize	= 6 // offset/0x40
-
-.const EOL_X_LO			= <320          // $40
-.const EOL_X_HI			= (>320) & 3    // $01
-
+.const arcadeRowSize		= 6 // offset/0x40
 .const hw_nmi_vec 		= $fffa
 .const hw_irq_vec 		= $fffe
-.const vicii_irqmask 		= $d01a
-.const ciaa_d 				= $dc0d
-.const ciab_d 				= $dd0d
+.const vicii_irqmask 	= $d01a
+.const ciaa_d 			= $dc0d
+.const ciab_d 			= $dd0d
 .const vicii_rcl 			= $d012
 .const vicii_rch 			= $d011
 .const vicii_irq			= $d019
@@ -70,4 +66,4 @@ byte 1 - character code LSB
 .const A_Register			= $f9
 .const Flags				= $fa
 .const tmp					= $fb
-
+.const tmp2				= $fc
