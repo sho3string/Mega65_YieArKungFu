d6ce5d5d8bcee26e4c88366307eb8f036eb37187
Samuel <36328867+sho3string@users.noreply.github.com>
2026-04-03 09:58:08 +1100

    Yiearkungfu.asm - Clean up
    Remove unwanted tables / arrays as a result of cleaning up SpriteQueue.asm

--- Source/asm/Yiearkungfu.asm
+++ Source/asm/Yiearkungfu.asm
@@ -7,22 +7,22 @@
 
 /* ZP */
 * = $200 "Basepage - yiearkungfu.asm" virtual
-	byte_0:	.byte	$00
-	byte_1:	.byte	$00
-	byte_2:	.byte	$00
-	byte_3:	.byte	$00
-	byte_4:	.byte	$00
-	byte_5:	.byte	$00
-	byte_6:	.byte	$00
-	byte_7:	.byte	$00
-	byte_8:	.byte	$00
-	byte_9:	.byte	$00
-	byte_a:	.byte	$00
-	byte_b:	.byte	$00
-	byte_c:	.byte	$00
-	byte_d:	.byte	$00
-	byte_e:	.byte	$00
-	byte_f:	.byte	$00
+	byte_0:		.byte	$00
+	byte_1:		.byte	$00
+	byte_2:		.byte	$00
+	byte_3:		.byte	$00
+	byte_4:		.byte	$00
+	byte_5:		.byte	$00
+	byte_6:		.byte	$00
+	byte_7:		.byte	$00
+	byte_8:		.byte	$00
+	byte_9:		.byte	$00
+	byte_a:		.byte	$00
+	byte_b:		.byte	$00
+	byte_c:		.byte	$00
+	byte_d:		.byte	$00
+	byte_e:		.byte	$00
+	byte_f:		.byte	$00
 	byte_10:	.byte	$00
 	byte_11:	.byte	$00
 	byte_12:	.byte	$00
@@ -266,6 +266,16 @@
 
 /* Constants */
 
+	// ZP layout
+.const sprPtrLo	= byte_30   // X pointer low
+.const sprPtrHi	= byte_31   // X pointer high
+.const logoPtrLo	= byte_46   // U pointer low
+.const logoPtrHi	= byte_47   // U pointer high
+.const logoX		= byte_21   // B equivalent
+.const logoCount	= byte_20   // Y equivalent
+.const tmpPtrLo 	= byte_f4
+.const tmpPtrHi 	= byte_f5
+
 #import "Source/asm/Constants.asm"
 
 
@@ -338,8 +348,7 @@ clearVars:
 	
 	jsr loc_8163	// start game.
 }
-.const SpriteQueueCount	= SpriteQueueBuffer + 0 // first byte is the count.
-.const SpriteQueueData	= SpriteQueueBuffer + 1 // data starts at second byte
+
 
 //#import "Source/asm/SidTest.asm"
 
@@ -389,17 +398,6 @@ buffer:
 	.fill (((SCREEN_WIDTH/8 * SCREEN_HEIGHT/8)* 2) + ((RRB_Tail_words*CHARS_HIGH*2))), 0
 buffer_buffer_end:
 
-
-
-* = * "Sprite Queue Buffer - yiearkungfu.asm"
-
-// --- Sprite Queue layout ---
-// [0] = count (0..SPRITE_COUNT)
-// [1..] = entries, 4 bytes each: Y, X, TILE, ATTR
-
-// extra headroom, use 128 instead of SPRITE_Q_SIZE.
-SpriteQueueBuffer:
-    .fill SPRITE_Q_SIZE, 0     // or: .fill SPRITE_Q_SIZE, 0
 
 #import "Source/asm/SpriteQueue.asm"
 #import "Source/asm/VideoRamHelper.asm"
